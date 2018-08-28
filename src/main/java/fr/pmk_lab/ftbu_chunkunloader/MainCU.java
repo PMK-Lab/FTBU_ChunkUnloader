61eb7469363b17f80fa86dd2d805e52568d2c3ce
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-08-28 20:12:11 +0200

    Ajout class MainCU avec methode main

---
+++ src/main/java/fr/pmk_lab/ftbu_chunkunloader/MainCU.java
@@ -0,0 +1,14 @@
+package fr.pmk_lab.ftbu_chunkunloader;
+
+public class MainCU {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Lancement FTBU_ChunkUnloader ");
+		
+	}
+
+}
