a23f119f7e8aa68b30733d6ad9e8a88a4b3be491
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-08-28 20:13:21 +0200

    Ajout class Config pour récupération des configurations de lancement

---
+++ src/main/java/fr/pmk_lab/ftbu_chunkunloader/config/Config.java
@@ -0,0 +1,23 @@
+package fr.pmk_lab.ftbu_chunkunloader.config;
+
+import java.io.File;
+
+import fr.pmk_lab.ftbu_chunkunloader.MainCU;
+
+public class Config {
+
+	private File f;
+	
+	public Config() {
+				
+		
+		
+	}
+
+	public File getFile() {
+		return f;
+	}
+	
+	
+	
+}
