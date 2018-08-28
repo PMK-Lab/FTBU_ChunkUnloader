3c564f784c14891998c0f8beab8054012b48d0bd
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-08-29 01:46:55 +0200

    Ajout récupération des configurations utilisateurs ( PropertiesData class )

--- src/main/java/fr/pmk_lab/ftbu_chunkunloader/MainCU.java
+++ src/main/java/fr/pmk_lab/ftbu_chunkunloader/MainCU.java
@@ -1,16 +1,39 @@
 package fr.pmk_lab.ftbu_chunkunloader;
 
 import fr.pmk_lab.ftbu_chunkunloader.data.InfoData;
+import fr.pmk_lab.ftbu_chunkunloader.data.PropertiesData;
 
 public class MainCU {
 	
-	public static final InfoData INFO = InfoData.getInfoData();
+	private static InfoData INFO;
+	private static PropertiesData PROPERTIES;
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Lancement FTBU_ChunkUnloader ");
 		
+		INFO = InfoData.getInfoData();
+		
+		try {
+			
+			PROPERTIES = PropertiesData.getPropertiesData();
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			System.out.println("Erreur de récupération des configurations ! EXIT");
+			System.exit(0);
+		}
+		
 	}
 
+	public static InfoData getINFO() {
+		return INFO;
+	}
+
+	public static PropertiesData getPROPERTIES() {
+		return PROPERTIES;
+	}
+	
 }
--- src/main/java/fr/pmk_lab/ftbu_chunkunloader/data/PropertiesData.java
+++ src/main/java/fr/pmk_lab/ftbu_chunkunloader/data/PropertiesData.java
@@ -1,11 +1,74 @@
 package fr.pmk_lab.ftbu_chunkunloader.data;
 
+import java.io.InputStream;
+import java.util.Properties;
+
+import fr.pmk_lab.ftbu_chunkunloader.MainCU;
+
 public class PropertiesData {
 
-	public PropertiesData() {
+	public static final String PROPERTIES_FILE_NAME = "config.properties";
+	
+	public static PropertiesData getPropertiesData() throws Exception {
+			
+		InputStream inputStream = MainCU.class.getClassLoader().getResourceAsStream(PROPERTIES_FILE_NAME);
+		Properties props = new Properties();
+			
+		props.load(inputStream);
+			
+		// get information in file info.properties
+		int cycle = Integer.parseInt(props.getProperty("cycle"));
 		
+		String wolrdName = props.getProperty("version");
 		
+		String libPath = props.getProperty("support");
+		String player = props.getProperty("forge_version_use_to_dev");
+		String team = props.getProperty("ftb_version_use_to_dev");
+			
+		//System.out.println(version + "\n" + support + "\n" + forge + "\n" + ftb);
+			
+		return new PropertiesData(cycle, wolrdName, libPath, player, team);
 		
+	}
+	
+	private int loadCycle;
+	
+	private String world;
+	private String ftbLibPath;
+	
+	private String playerPath;
+	private String teamPath;
+	
+	public PropertiesData(int lc , String w , String ftblp , String pp , String tp) {
+		
+		this.loadCycle = lc;
+		
+		this.world = w;
+		this.ftbLibPath = ftblp;
+		
+		this.playerPath = pp;
+		this.teamPath = tp;
+		
+	}
+
+	public int getLoadCycle() {
+		return loadCycle;
+	}
+
+	public String getWorld() {
+		return world;
+	}
+
+	public String getFtbLibPath() {
+		return ftbLibPath;
+	}
+
+	public String getPlayerPath() {
+		return playerPath;
+	}
+
+	public String getTeamPath() {
+		return teamPath;
 	}
 	
 }
