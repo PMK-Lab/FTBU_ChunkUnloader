332583c4e65b739ebe0954b183131284e2d0a9e9
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-08-29 01:05:01 +0200

    Ajout class d'information

--- src/main/java/fr/pmk_lab/ftbu_chunkunloader/data/InfoData.java
+++ src/main/java/fr/pmk_lab/ftbu_chunkunloader/data/InfoData.java
@@ -2,10 +2,36 @@ package fr.pmk_lab.ftbu_chunkunloader.data;
 
 public class InfoData {
 
-	public InfoData() {
+	private String version;
+	private String supportLink;
+	
+	private String forgeVersion;
+	private String FtbVersion;
+	
+	public InfoData(String v , String s , String fv , String ftbv) {
 		
+		this.version = v;
+		this.supportLink = s;
 		
+		this.forgeVersion = fv;
+		this.FtbVersion = ftbv;
 		
+	}
+	
+	public String getVersion() {
+		return version;
+	}
+
+	public String getSupportLink() {
+		return supportLink;
+	}
+
+	public String getForgeVersion() {
+		return forgeVersion;
+	}
+
+	public String getFtbVersion() {
+		return FtbVersion;
 	}
 	
 }
