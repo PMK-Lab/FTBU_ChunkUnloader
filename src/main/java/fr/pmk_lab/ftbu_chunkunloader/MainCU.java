317961f765356d34918061b685cdd34c70ce8727
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-08-29 01:17:35 +0200

    Ajout récupération et method de récupération info file

--- src/main/java/fr/pmk_lab/ftbu_chunkunloader/MainCU.java
+++ src/main/java/fr/pmk_lab/ftbu_chunkunloader/MainCU.java
@@ -1,6 +1,10 @@
 package fr.pmk_lab.ftbu_chunkunloader;
 
+import fr.pmk_lab.ftbu_chunkunloader.data.InfoData;
+
 public class MainCU {
+	
+	public static final InfoData INFO = InfoData.getInfoData();
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
--- src/main/java/fr/pmk_lab/ftbu_chunkunloader/data/InfoData.java
+++ src/main/java/fr/pmk_lab/ftbu_chunkunloader/data/InfoData.java
@@ -1,7 +1,42 @@
 package fr.pmk_lab.ftbu_chunkunloader.data;
 
+import java.io.InputStream;
+import java.util.Properties;
+
 public class InfoData {
 
+	public static final String INFO_FILE_NAME = "info.properties";
+	
+	public static InfoData getInfoData() {
+		
+		try {
+			
+			InputStream inputStream = ClassLoader.getSystemResourceAsStream(INFO_FILE_NAME);
+			Properties props = new Properties();
+			
+			props.load(inputStream);
+			
+			// get information in file info.properties
+			String version = props.getProperty("version");
+			String support = props.getProperty("support");
+			String forge = props.getProperty("forge_version_use_to_dev");
+			String ftb = props.getProperty("ftb_version_use_to_dev");
+			
+			System.out.println(version + "\n" + support + "\n" + forge + "\n" + ftb);
+			
+			return new InfoData(version, support, forge, ftb);
+			
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+			
+		}
+		
+		// return default values for InfoData
+		return new InfoData("default_error", "default_error" , "default_error" , "default_error");
+		
+	}
+	
 	private String version;
 	private String supportLink;
 	
