b3bc2c06ec1df852450275be15d57d8d46149c49
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-08-29 20:30:09 +0200

    Ajout methode récupération WorldDayTime à partir de Properties

--- src/main/java/fr/pmk_lab/ftbu_chunkunloader/ChunkUnloaderManager.java
+++ src/main/java/fr/pmk_lab/ftbu_chunkunloader/ChunkUnloaderManager.java
@@ -1,7 +1,37 @@
 package fr.pmk_lab.ftbu_chunkunloader;
 
+import java.io.File;
+
+import fr.pmk_lab.ftbu_chunkunloader.data.PropertiesData;
+
 public class ChunkUnloaderManager {
 
-	//public static Ch
+	private static long worldDayTime;
+	
+	public static long getWorldDayTime(PropertiesData p) throws Exception {
+		
+		String pathFile = p.getWorld() + "/level.dat";
+		
+		File f = new File(pathFile);
+		
+		if(f.exists()) {
+			
+			return NBTUtils.getWorldDayTime(f);
+			
+		}else {
+			
+			throw new Exception("Fichier non trouvé : " + pathFile + "\n Impossible de récupérer les informations neccésaires au unload.");
+			
+		}
+		
+	}
+
+	public static long getWorldDayTime() {
+		return worldDayTime;
+	}
+
+	public static void setWorldDayTime(long worldDayTime) {
+		ChunkUnloaderManager.worldDayTime = worldDayTime;
+	}
 	
 }
