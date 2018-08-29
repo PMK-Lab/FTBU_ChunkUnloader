d47255d0478160f1d407792d11c5ba2ff1ded36a
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-08-29 20:28:37 +0200

    Ajout method lecture nbt world DayTime

--- src/main/java/fr/pmk_lab/ftbu_chunkunloader/NBTUtils.java
+++ src/main/java/fr/pmk_lab/ftbu_chunkunloader/NBTUtils.java
@@ -1,43 +1,27 @@
 package fr.pmk_lab.ftbu_chunkunloader;
 
 import java.io.File;
+import java.io.IOException;
+
+import com.kovuthehusky.nbt.NBTReader;
+import com.kovuthehusky.nbt.tags.NBTCompound;
+import com.kovuthehusky.nbt.tags.NBTLong;
 
 public class NBTUtils {
 
-	public static void getTeamList() {
+	public static long getWorldDayTime(File f) throws IOException {
 		
+		NBTCompound nbt = NBTReader.read(f);
 		
+		nbt = nbt.get("Data");
+		nbt = new NBTCompound("Data", nbt.getPayload());
 		
-	}
-	
-	public static void getPlayerList() {
+		NBTLong nbtL = nbt.get("DayTime");
 		
-		
-		
-	}
-	
-	public static Object getPlayerNBT(String playerName) {
-		
-		return getPlayerNBT(new File(playerName + ".dat")); //return nbt from player
+		return nbtL.getPayload();
 		
 	}
 	
-	public static Object getPlayerNBT(File f) {
-		
-		return null; //return nbt from player
-		
-	}
 	
-	public static int getPlayerLastSeen() {
-		
-		return 0;
-		
-	}
-	
-	public static String getPlayerGradeName() {
-		
-		return "";
-		
-	}
 	
 }
