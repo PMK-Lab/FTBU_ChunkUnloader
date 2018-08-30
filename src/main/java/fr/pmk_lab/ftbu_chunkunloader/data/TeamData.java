3bdce78d8b2d011011094c1939d9430ea8215fc3
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-08-30 18:41:02 +0200

    Ajout field & update constructeur

--- src/main/java/fr/pmk_lab/ftbu_chunkunloader/data/TeamData.java
+++ src/main/java/fr/pmk_lab/ftbu_chunkunloader/data/TeamData.java
@@ -1,5 +1,7 @@
 package fr.pmk_lab.ftbu_chunkunloader.data;
 
+import java.util.List;
+
 import com.kovuthehusky.nbt.tags.NBTCompound;
 
 public class TeamData {
@@ -7,12 +9,33 @@ public class TeamData {
 	//nbt compound
 	private NBTCompound fileNbt;
 	
+	private String fileName;
+	private String teamName;
+	
+	private PlayerData owner;
+	private List<PlayerData> players;	
+	
 	public TeamData(NBTCompound nbt) {
 		// TODO Auto-generated constructor stub
 		
 		this.fileNbt = nbt;
 		
+		this.fileName = nbt.getName();
 		
+		//récupération du nom
+		String t = (String) nbt.get("Title").getPayload();
+		
+		if(t.isEmpty()) {
+			
+			this.teamName = this.fileName;
+			
+		}else {
+			this.teamName = t;
+		}
+		
+		//récupération du nom du propiétaire
+		
+		//owner = NBTUtils.getNbtFromFile(f)
 		
 	}
 
