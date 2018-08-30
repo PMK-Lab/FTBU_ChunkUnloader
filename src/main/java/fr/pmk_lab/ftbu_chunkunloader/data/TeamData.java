22e97ad1b5926e414ab6dc812239edd9fa778695
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-08-30 13:21:10 +0200

    Ajout constructeur dans TeamData

--- src/main/java/fr/pmk_lab/ftbu_chunkunloader/data/TeamData.java
+++ src/main/java/fr/pmk_lab/ftbu_chunkunloader/data/TeamData.java
@@ -1,5 +1,11 @@
 package fr.pmk_lab.ftbu_chunkunloader.data;
 
+import com.kovuthehusky.nbt.tags.NBTCompound;
+
 public class TeamData {
+
+	public TeamData(NBTCompound nbt) {
+		// TODO Auto-generated constructor stub
+	}
 
 }
