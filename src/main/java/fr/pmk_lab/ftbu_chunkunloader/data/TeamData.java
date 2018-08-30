7a6779e102f2ab8c8f92b204163510294c0682d7
Clem-Fern-Dev <clement.coussieu@gmail.com>
2018-08-30 16:01:06 +0200

    UPdate TeamData (save)

--- src/main/java/fr/pmk_lab/ftbu_chunkunloader/data/TeamData.java
+++ src/main/java/fr/pmk_lab/ftbu_chunkunloader/data/TeamData.java
@@ -4,8 +4,24 @@ import com.kovuthehusky.nbt.tags.NBTCompound;
 
 public class TeamData {
 
+	//nbt compound
+	private NBTCompound fileNbt;
+	
 	public TeamData(NBTCompound nbt) {
 		// TODO Auto-generated constructor stub
+		
+		this.fileNbt = nbt;
+		
+		
+		
+	}
+
+	public NBTCompound getFileNbt() {
+		return fileNbt;
+	}
+
+	public void setFileNbt(NBTCompound fileNbt) {
+		this.fileNbt = fileNbt;
 	}
 
 }
